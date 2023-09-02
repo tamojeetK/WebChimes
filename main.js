692c098b624e5131bcf3934376a0a8aa8b81e64f
tamojeetK <tamojeet.12012003@lpu.in>
2023-09-02 23:54:47 +0530

    -update key pressing functionality -added effects

--- main.js
+++ main.js
@@ -1,7 +1,34 @@
-document.addEventListener("keydown", function(e) {
+document.addEventListener("keydown", function (e) {
+    const keyPress = document.querySelector(`.main-keys[data-key="${e.key}"]`);
     const audioSource = document.querySelector(`audio[data-key="${e.key}"]`);
-    if(!audioSource) return;
-    console.log(e);
+
+    if (!audioSource) return;
     audioSource.currentTime = 0;
     audioSource.play();
+
+    keyPress.classList.add("pressed");
+
 });
+
+document.addEventListener("keyup", function (e) {
+    const keyPress = document.querySelector(`.main-keys[data-key="${e.key}"]`);
+    if (!keyPress) return;
+    keyPress.classList.remove("pressed");
+});
+
+
+// ------------============for MOUSE Click============------------
+// const keys = document.querySelectorAll('.main-keys');
+// keys.forEach(key => {
+//     key.addEventListener('click', function () {
+//         const keyPress = this;
+//         const audioSource = document.querySelector(`audio[data-key="${keyPress.getAttribute('data-key')}"]`);
+
+//         if (!audioSource) return;
+//         audioSource.currentTime = 0;
+//         audioSource.play();
+
+//         keyPress.classList.add("pressed");
+//     });
+
+// });
